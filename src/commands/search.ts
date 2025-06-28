ee181e4a93b424c0cacd0efe3f48fb2486ff17aa
astrid <git@astrid.email>
2025-06-29 01:49:38 +0200

    pass info and newest args to get command from search command

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,31 +1,15 @@
-import { z } from 'zod';
-import { tupleWithOptional } from 'zod-tuple-with-optional';
-
 import { createCommand } from '@/commands';
 import { command as getCommand } from '@/commands/get';
-import { MEDIA_NAME_REGEX } from '@/constants';
 
 export const command = createCommand({
   command: 'search',
   aliases: ['find', 'f'],
   description: 'Search media',
-  args: {
-    schema: z.object({
-      _: tupleWithOptional([
-        z
-          .string()
-          .regex(MEDIA_NAME_REGEX, {
-            message: 'Search value contains invalid characters',
-          })
-          .optional()
-          .describe('Search value'),
-      ]).default([undefined]),
-    }),
-  },
+  args: getCommand.args,
   async handleCommand(message, args, commands) {
     await getCommand.handleCommand(
       message,
-      getCommand.args!.schema.parse({}),
+      { ...args, _: [undefined] },
       commands,
       {
         search: true,
